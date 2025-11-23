8ad9d207abaa7d937ba57e15a3df165941ef9716
lifochiu <lifochiu.ca@gmail.com>
2025-11-23 16:27:29 -0500

    website update

--- script/global.js
+++ script/global.js
@@ -1,5 +1,10 @@
 const menuImg = document.getElementById('menuImg');
 const popUpMenu = document.getElementById('popUpMenu');
+// const time = new Date();
+const search = document.getElementById('search');
+const searchBar = document.getElementById('searchArea');
+// let searchBarDisplay = false;
+// searchBar.style.display = "none";
 
 menuImg.addEventListener('click', () => {
     let menuStatus = window.getComputedStyle(popUpMenu).visibility;
@@ -9,4 +14,8 @@ menuImg.addEventListener('click', () => {
         popUpMenu.style.visibility = "hidden"
     }
 });
+// document.getElementById('currentTime').innerText = time;
 
+search.addEventListener('click', () => {  
+    searchBar.classList.toggle('active');
+})
